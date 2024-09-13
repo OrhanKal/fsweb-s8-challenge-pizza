aa28867fb386cd3b10c995f4c17787b7cf55d754
OrhanKal <kalsinorhan@gmail.com>
2024-09-13 19:05:05 +0300

    add home and success css

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { Button } from 'reactstrap'
 function Home() {
     return (
-        <div> asddasda
-            <Link to={`/form`}>
-                <button type='button'>ACIKTIM</button>
-            </Link>
-        </div>
+        <header>
+            <div className='main-hero'>
+                <h1>Teknolojik Yemekler</h1>
+                <p>KOD ACIKTIRIR<br />Pİzza, DOYURUR</p>
+                <div >
+                    <Link to={`/form`}>
+                        <Button className='home-button' type='button'>ACIKTIM</Button>
+                    </Link>
+                </div>
+            </div>
+        </header>
     )
 }
 
--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 
 function Success() {
     return (
-        <div>
-            Success
-        </div>
+        <>
+            <div className='main-hero-success'>
+                <h1>Teknolojik Yemekler</h1>
+                <p>TEBRİKLER!<br />SİPARİŞİNİZ ALINDI!</p>
+            </div>
+        </>
     )
 }
 
